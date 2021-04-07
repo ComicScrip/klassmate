323165ea857f8ec101ad0e4598fa310656b82ddf
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-04-07 15:39:06 +0200

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import Button from '@material-ui/core/Button';
 import Avatar from './components/Avatar';
 import Header from './components/Header';
+import './index.css';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        Klassmate
+        <p className="text-red-700">Klassmate</p>
+        <Button variant="contained" color="secondary">
+          Hello World
+        </Button>
         <Avatar
           avatarUrl="https://randomuser.me/api/portraits/women/25.jpg"
           alt="user avatar"
