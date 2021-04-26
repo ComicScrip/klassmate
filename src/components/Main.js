8c7b55632582a3f775fa50c0d111277815bbe861
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-04-26 18:29:52 +0200

    basic read and create

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import { Route, Switch } from 'react-router';
 import ActivityPage from '../screens/ActivityPage';
 import CreateGroupsPages from '../screens/CreateGroupsPage';
+import CreateNotePage from '../screens/CreateNotePage';
 import DojoPage from '../screens/DojoPage';
 import LoginPage from '../screens/LoginPage';
+import NotesPage from '../screens/NotesPage';
 import ProfilePage from '../screens/ProfilePage';
 
 export default function Main() {
@@ -14,6 +16,8 @@ export default function Main() {
         <Route exact path="/createGroups" component={CreateGroupsPages} />
         <Route exact path="/dojo" component={DojoPage} />
         <Route path="/activity" component={ActivityPage} />
+        <Route exact path="/notes" component={NotesPage} />
+        <Route exact path="/notes/new" component={CreateNotePage} />
       </Switch>
     </main>
   );
---
+++ src/screens/NotesPage.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import API from '../APIClient';
+
+export default function NotesPage() {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    API.get('/notes').then((res) => setNotes(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-3xl text-center">Notes</h2>
+      <Link to="/notes/new">Add new note</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notes.map((note) => {
+            return (
+              <tr>
+                <td>{note.title}</td>
+                <td>edit, delete</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
