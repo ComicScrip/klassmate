82051e3efe3224890d79ab96c760bce60bb02b81
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-07-08 19:19:17 +0200

    improvements on dojo page

--- src/screens/DojoPage.js
+++ src/screens/DojoPage.js
@@ -1,36 +1,40 @@
+import { v4 as uuid } from 'uuid';
+import { Button, TextField, makeStyles } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useTransition, animated } from 'react-spring';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { useEffect, useState } from 'react';
+import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import StopIcon from '@material-ui/icons/Stop';
+import RotateRightIcon from '@material-ui/icons/RotateRight';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import Avatar from '../components/Avatar';
+import API from '../APIClient';
 
 dayjs.extend(duration);
 
-const initialTeam = [
-  {
-    firstName: 'Jeanne',
+const useStyles = makeStyles({
+  noOptions: {
+    display: 'none',
   },
-  {
-    firstName: 'Pierre',
-  },
-  {
-    firstName: 'Paulette',
-  },
-  {
-    firstName: 'Jacques',
-  },
-];
+});
 
 export default function DojoPage() {
   const initialTimerValue = 15;
-  const [team, setTeam] = useState(initialTeam);
-  const [pilot, copilot, ...otherMembers] = team;
+  const [studentsLoading, setStudentsLoading] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [team, setTeam] = useState([]);
   const [secondsLeft, setSecondsLeft] = useState(initialTimerValue);
   const [chronoStarted, setChronoStarted] = useState(false);
+  const [usersToAddToTeam, setUsersToAddToTeam] = useState([]);
+  const [autocompleteInputText, setAutocompleteInputText] = useState('');
+  const classes = useStyles();
 
-  const [newStudentName, setNewStudentName] = useState('');
-
-  const handleNewStudentNameChange = (e) => {
-    setNewStudentName(e.target.value);
-  };
+  const handleTeamMemberRemoval = (id) =>
+    setTeam((currentTeam) => currentTeam.filter((member) => member.id !== id));
 
   const handleTeamRotation = () => {
     // setTeam(([first, ...others]) => [...others, first]);
@@ -42,6 +46,18 @@ export default function DojoPage() {
   };
 
   useEffect(() => {
+    setStudentsLoading(true);
+    API.get('/students')
+      .then((res) => {
+        setStudents(res.data);
+      }) // eslint-disable-next-line
+      .catch(console.error)
+      .finally(() => {
+        setStudentsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
     let timerId = null;
 
     if (chronoStarted) {
@@ -50,10 +66,7 @@ export default function DojoPage() {
       }, 1000);
     }
 
-    window.console.log('in useEffect, timer id :', timerId);
-
     return () => {
-      window.console.log('cleaning timer id : ', timerId);
       if (timerId) clearInterval(timerId);
     };
   }, [chronoStarted]);
@@ -67,36 +80,159 @@ export default function DojoPage() {
 
   const handleNewStudentAddition = (e) => {
     e.preventDefault();
-    setTeam([...team, { firstName: newStudentName }]);
-    setNewStudentName('');
+    if (autocompleteInputText) {
+      setTeam([...team, { firstName: autocompleteInputText, id: uuid() }]);
+      setAutocompleteInputText('');
+    }
   };
+
+  const rowHeight = 50;
+  const transitions = useTransition(
+    team.map((data, i) => ({ ...data, y: rowHeight * i })),
+    {
+      key: (item) => item.id,
+      from: { opacity: 0, width: '0%', height: 0 },
+      leave: { opacity: 0, height: 0 },
+      enter: ({ y }) => ({
+        y,
+        width: '100%',
+        opacity: 1,
+        height: rowHeight,
+      }),
+      update: ({ y, height }) => ({ y, height }),
+    }
+  );
+
+  const handleAddMembers = () => {
+    setTeam((currentTeam) => [...currentTeam, ...usersToAddToTeam]);
+    setUsersToAddToTeam([]);
+  };
+
+  const selectableNewMembers = students.filter(
+    (s) => !team.map((m) => m.id).includes(s.id)
+  );
 
   return (
     <>
-      <button type="button" onClick={handleChronoToggle}>
-        {chronoStarted ? 'Stop' : 'Start'}
-      </button>
-      <button type="button" onClick={handleTeamRotation}>
-        Team Rotation
-      </button>
-      <div className="text-xl">
+      <div className="flex justify-between mt-5">
+        <Button
+          onClick={handleChronoToggle}
+          variant="contained"
+          color={chronoStarted ? 'secondary' : 'primary'}
+          startIcon={
+            chronoStarted ? <StopIcon /> : <PlayCircleFilledWhiteIcon />
+          }
+        >
+          {chronoStarted ? 'Stop' : 'Start'}
+        </Button>
+        <Button
+          onClick={handleTeamRotation}
+          variant="contained"
+          color="primary"
+          disabled={chronoStarted}
+          startIcon={<RotateRightIcon />}
+        >
+          Rotate Team
+        </Button>
+      </div>
+      <div className="text-3xl m-5 text-center">
         {dayjs.duration(secondsLeft, 'seconds').format('mm:ss')}
       </div>
-      <div className="bg-yellow-600">{pilot.firstName}</div>
-      <div className="bg-yellow-300">{copilot.firstName}</div>
-      {otherMembers.map((member) => (
-        <div key={member.firstName} className="bg-blue-100">
-          {member.firstName}
-        </div>
-      ))}
-      <form onSubmit={handleNewStudentAddition}>
-        <input
-          type="text"
-          placeholder="Add new student"
-          value={newStudentName}
-          onChange={handleNewStudentNameChange}
+
+      <div
+        style={{
+          height: rowHeight * team.length,
+          transition: 'height 0.5s ease-out',
+        }}
+        className="relative rounded-md overflow-hidden"
+      >
+        {transitions(({ y, ...rest }, item, t, index) => {
+          let bgColor = 'bg-blue-100';
+          if (index === 0) bgColor = 'bg-yellow-500';
+          if (index === 1) bgColor = 'bg-yellow-300';
+
+          return (
+            <animated.div
+              key={item.key}
+              style={{
+                transform: y.interpolate((v) => `translate3d(0,${v}px,0)`),
+                height: rowHeight,
+                position: 'absolute',
+                willChange: 'transform, height, opacity, background-color',
+                transition: 'background-color 1s ease-out',
+                width: '100%',
+                ...rest,
+              }}
+              className={`${bgColor} p-3 overflow-hidden shadow-lg ${
+                index === team.length - 1 ? '' : 'border-b border-gray-500'
+              }`}
+            >
+              <div className="flex h-full items-center justify-between">
+                <div className="flex">
+                  <Avatar avatarUrl={item.avatarUrl} size={30} />
+                  <p className="ml-3">{item.firstName}</p>
+                </div>
+
+                <IconButton onClick={() => handleTeamMemberRemoval(item.id)}>
+                  <RemoveCircleIcon color="action" />
+                </IconButton>
+              </div>
+            </animated.div>
+          );
+        })}
+      </div>
+
+      <form
+        autoComplete="off"
+        onSubmit={handleNewStudentAddition}
+        className="mt-10 flex justify-between h-13"
+      >
+        <Autocomplete
+          value={usersToAddToTeam}
+          id="user-select"
+          fullWidth
+          options={studentsLoading ? [] : selectableNewMembers}
+          autoHighlight
+          getOptionLabel={(option) => option.firstName}
+          multiple
+          classes={{
+            noOptions: classes.noOptions,
+          }}
+          renderOption={(option) => (
+            <div className="flex items-center">
+              <Avatar size={30} avatarUrl={option.avatarUrl} />
+              <p className="ml-3">{option.firstName}</p>
+            </div>
+          )}
+          disableCloseOnSelect
+          renderInput={(params) => (
+            <TextField
+              fullWidth
+              {...params}
+              onChange={(e) => {
+                setAutocompleteInputText(e.target.value);
+              }}
+              label="Add team members"
+              variant="outlined"
+              inputProps={{
+                ...params.inputProps,
+                value: autocompleteInputText,
+                autoComplete: 'off',
+              }}
+            />
+          )}
+          onChange={(e, selectedUsers) => setUsersToAddToTeam(selectedUsers)}
         />
-        <button type="submit">Add</button>
+        <div className="ml-3">
+          <Button
+            className="h-full"
+            onClick={handleAddMembers}
+            variant="contained"
+            color="primary"
+            disabled={chronoStarted}
+            startIcon={<AddIcon />}
+          />
+        </div>
       </form>
     </>
   );
